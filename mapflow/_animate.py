b33f5183fc18c37f8b19788ba008cb27aef947e2
CyrilJl <joly.cyril7@gmail.com>
2025-05-24 11:42:55 +0200

    fix: streamline timeout handling in video generation

--- mapflow/_animate.py
+++ mapflow/_animate.py
@@ -139,11 +139,11 @@ class Animation:
                         total=len(data),
                         disable=(not self.verbose),
                         desc="Frames generation",
+                        leave=False,
                     )
                 )
 
-            if timeout == "auto":
-                timeout = min(10, 0.1 * len(data))
+            timeout = min(10, 0.1 * len(data)) if timeout == "auto" else timeout
             # ffmpeg command to create the video
             self._create_video(tempdir, path, fps, timeout=timeout)
 
